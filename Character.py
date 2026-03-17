f7564f7aeb168422c20a6103e6faed226821a8b4
Montessa Young <montessakayyoung@gmail.com>
2026-03-17 14:43:18 +0000

    fixed character file

--- Character.py
+++ Character.py
@@ -1,107 +1,95 @@
-dict_demo = {
-    "creature": "Bigfoot",
-    "Color": "brown",
-    "strength": "100%",
-    "Weakness": "Cheese",
-    "height": "15 ft",
-    "Health": "150"
-}
-for key, value in dict_demo.items():
-    print(f"{key}: {value}")
-print(len(dict_demo))
+class Character:
+    def __init__(self, Creature: str, Color: str, Strength: int, Weakness: str, Height:int, Health:int):
+        self.Creature = Creature
+        self.Color = Color
+        self.Strength = Strength
+        self.Weakness = Weakness
+        self.Height = Height
+        self.Health = Health
 
-dict_demo = {
+    def __repr__(self):
+        return f"Character(Creature={self.Creature}, Color= {self.Color}, Strength = {self.Strength}, Weakness = {self.Weakness}, Height = {self.Height}, Health = {self.Health})"
+
+
+# a small roster of snakes the player can choose from
+AVAILABLE_CHARACTERS = [{
+    "Creature": "Bigfoot",
+    "Color": "Brown",
+    "Strength": 50,
+    "Weakness": "Cheese",
+    "Height": 15,
+    "Health": 150
+},
+
+{
     "Creature": "Yeti",
     "Color": "White",
-    "strength": "resilience",
+    "Strength": 100 ,
     "Weakness": "Impatience",
-    "Height": "15 ft",
-    "Health": "150"
-}
-for key, value in dict_demo.items():
-    print(f"{key}: {value}")
-print(len(dict_demo))
-
-dict_demo = {
-    "Creatures": "Sled dogs",
-    "color": "black and white",
-    "strength": "pulling sled",
-    "weakness": "fish",
-    "Height": "10 ft",
-    "Health": "2000"
-}
-for key, value in dict_demo.items():
-    print(f"{key}: {value}")
-print(len(dict_demo))
-
-dict_demo = {
-    "Character": "Lloyd Christmas",
-    "Gender": "Male",
-    "strength": "Stupidity",
-    "weakness": "Intellegience",
-    "Height": "8 ft",
-    "Health": "100"
-}
-for key, value in dict_demo.items():
-    print(f"{key}: {value}")
-print(len(dict_demo))
-
-dict_demo = {
-    "Character": "Lara Croft",
-    "Gender": "Female",
-    "strength": "Explorer",
-    "weakness": "Human mortality",
-    "Height": "8 ft",
-    "Health": "100"
-}
-for key, value in dict_demo.items():
-    print(f"{key}: {value}")
-print(len(dict_demo))
-
-dict_demo = {
-    "Creatures": "Ice Minotuar",
-    "color": "Gray and White",
-    "strength": "Guarding",
+    "Height": 15,
+    "Health": 150
+},
+{
+    "Creature": "Sled dogs",
+    "Color": "black and white",
+    "Strength": 40,
+    "Weakness": "fish",
+    "Height": 10,
+    "Health": 2000
+},
+{
+    "Creature": "Lloyd Christmas",
+    "Color": "Tan",
+    "Strength": 5,
+    "Weakness": "Intellegience",
+    "Height": 8,
+    "Health": 100
+},
+{
+    "Creature": "Lara Croft",
+    "Color": "Black",
+    "Strength": 20,
+    "Weakness": "Human mortality",
+    "Height": 8,
+    "Health": 100
+},
+{
+    "Creature": "Ice Minotuar",
+    "Color": "Gray and White",
+    "Strength": 60,
     "weakness": "Color Red",
-    "Height": "15 ft",
-    "Health": "200"
+    "Height": 15,
+    "Health": 200
+},
+{
+    "Creature": "Dragon",
+    "Color": "Striking Red",
+    "Strength": 200,
+    "Weakness": "Fire Sword",
+    "Height": 15,
+    "Health": 200
+},
+{
+    "Creature": "Python Snake",
+    "Color": "Yellow",
+    "Strength": 140,
+    "Weakness": "Smoke Grenade",
+    "Height": 15,
+    "Health": 200
+},
+{
+    "Creature": "Vampire",
+    "Color": "Dark Grey",
+    "Strength": 160,
+    "Weakness": "Wooden Cross",
+    "Height": 15,
+    "Health": 200
 }
-for key, value in dict_demo.items():
-    print(f"{key}: {value}")
-print(len(dict_demo))
+]
 
-dict_demo = {
-    "Character": "Dragon",
-    "Gender": "Female",
-    "strength": "Flying and Fire Breathing",
-    "weakness": "Fire Sword",
-    "Height": "15 ft",
-    "Health": "200"
-}
-for key, value in dict_demo.items():
-    print(f"{key}: {value}")
-print(len(dict_demo))
 
-dict_demo = {
-    "Character": "Python Snake",
-    "Gender": "Male",
-    "strength": "Hiding",
-    "weakness": "Smoke Grenade",
-    "Height": "15 ft",
-    "Health": "200"
-}
-for key, value in dict_demo.items():
-    print(f"{key}: {value}")
-print(len(dict_demo))
 
-dict_demo = {
-    "Character": "Vampire",
-    "Gender": "Female",
-    "strength": "Speed",
-    "weakness": "Wooden Cross",
-    "Height": "15 ft",
-    "Health": "200"
-}
-for key, value in dict_demo.items():
-    print(f"{key}: {value}")
-print(len(dict_demo))
+
+
+
+
